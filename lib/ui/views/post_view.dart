a7bf75f6c8f7e49c529a7975f83a40a1ee025e30
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-21 21:53:04 +0000

    Add screens

---
+++ lib/ui/views/post_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutterproviderarchitecture/core/models/post.dart';
+
+class PostView extends StatelessWidget {
+  final Post post;
+
+  const PostView({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
