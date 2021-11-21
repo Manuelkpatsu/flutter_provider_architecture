b18228782f0733f2e97cea11fd83dba4dcf8e30e
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-21 22:38:39 +0000

    Add widgets

---
+++ lib/ui/widgets/postList_item.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:flutterproviderarchitecture/core/models/post.dart';
+
+class PostListItem extends StatelessWidget {
+  final Post post;
+  final VoidCallback onTap;
+
+  const PostListItem({Key? key, required this.post, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5.0),
+            boxShadow: const [
+              BoxShadow(
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 2.0),
+                  color: Color.fromARGB(80, 0, 0, 0))
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              post.title,
+              style: const TextStyle(fontWeight: FontWeight.w900, fontSize: 16.0),
+            ),
+            Text(post.body, maxLines: 2, overflow: TextOverflow.ellipsis)
+          ],
+        ),
+      ),
+    );
+  }
+}
